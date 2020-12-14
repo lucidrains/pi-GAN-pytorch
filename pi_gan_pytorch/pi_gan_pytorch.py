a5ee8d7f78057e9a9c0a7295b89ee3da5167c355
Phil Wang <lucidrains@gmail.com>
2020-12-14 15:06:05 -0800

    first commit for generator - Siren network FiLM modulated with latent from mapping network, a la Stylegan

--- pi_gan_pytorch/pi_gan_pytorch.py
+++ pi_gan_pytorch/pi_gan_pytorch.py
@@ -1,11 +1,163 @@
+import math
 import torch
-import torch.nn.functional as F
-from einops import rearrange
 from torch import nn, einsum
+import torch.nn.functional as F
 
-class piGAN(nn.Module):
-    def __init__(self):
+from einops import rearrange, repeat
+
+# helper
+
+def exists(val):
+    return val is not None
+
+# sin activation
+
+class Sine(nn.Module):
+    def __init__(self, w0 = 1.):
+        super().__init__()
+        self.w0 = w0
+    def forward(self, x):
+        return torch.sin(self.w0 * x)
+
+# siren layer
+
+class Siren(nn.Module):
+    def __init__(self, dim_in, dim_out, w0 = 30., c = 6., is_first = False, use_bias = True, activation = None):
+        super().__init__()
+        self.dim_in = dim_in
+        self.is_first = is_first
+
+        weight = torch.zeros(dim_out, dim_in)
+        bias = torch.zeros(dim_out) if use_bias else None
+        self.init_(weight, bias, c = c, w0 = w0)
+
+        self.weight = nn.Parameter(weight)
+        self.bias = nn.Parameter(bias) if use_bias else None
+        self.activation = Sine(w0) if activation is None else activation
+
+    def init_(self, weight, bias, c, w0):
+        dim = self.dim_in
+
+        w_std = (1 / dim) if self.is_first else (math.sqrt(c / dim) / w0)
+        weight.uniform_(-w_std, w_std)
+
+        if bias is not None:
+            bias.uniform_(-w_std, w_std)
+
+    def forward(self, x, gamma = None, beta = None):
+        out =  F.linear(x, self.weight, self.bias)
+
+        # FiLM modulation
+
+        if exists(gamma):
+            out = out * gamma[:, None, :]
+
+        if exists(beta):
+            out = out + beta[:, None, :]
+
+        out = self.activation(out)
+        return out
+
+# mapping network
+
+class EqualLinear(nn.Module):
+    def __init__(self, in_dim, out_dim, lr_mul = 1, bias = True):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(out_dim, in_dim))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_dim))
+
+        self.lr_mul = lr_mul
+
+    def forward(self, input):
+        return F.linear(input, self.weight * self.lr_mul, bias=self.bias * self.lr_mul)
+
+class MappingNetwork(nn.Module):
+    def __init__(self, *, dim, dim_out, depth = 8, lr_mul = 0.1):
         super().__init__()
 
+        layers = []
+        for i in range(depth):
+            layers.extend([EqualLinear(dim, dim, lr_mul), nn.LeakyReLU(0.1)])
+
+        self.net = nn.Sequential(*layers)
+
+        self.to_gamma = nn.Linear(dim, dim_out)
+        self.to_beta = nn.Linear(dim, dim_out)
+
     def forward(self, x):
-        return x
+        x = F.normalize(x, dim = 1)
+        x = self.net(x)
+        return self.to_gamma(x), self.to_beta(x)
+
+# siren network
+
+class SirenNet(nn.Module):
+    def __init__(self, dim_in, dim_hidden, dim_out, num_layers, w0 = 30., w0_initial = 30., use_bias = True, final_activation = None):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+
+        for ind in range(num_layers):
+            is_first = ind == 0
+            layer_w0 = w0_initial if is_first else w0
+            layer_dim_in = dim_in if is_first else dim_hidden
+
+            self.layers.append(Siren(
+                dim_in = layer_dim_in,
+                dim_out = dim_hidden,
+                w0 = layer_w0,
+                use_bias = use_bias,
+                is_first = is_first
+            ))
+
+        self.last_layer = Siren(dim_in = dim_hidden, dim_out = dim_out, w0 = w0, use_bias = use_bias, activation = final_activation)
+
+    def forward(self, x, gamma, beta):
+        for layer in self.layers:
+            x = layer(x, gamma, beta)
+        return self.last_layer(x)
+
+# generator
+
+class Generator(nn.Module):
+    def __init__(self, image_size, dim, dim_hidden):
+        super().__init__()
+        self.mapping = MappingNetwork(
+            dim = dim,
+            dim_out = dim_hidden
+        )
+
+        self.siren = SirenNet(
+            dim_in = 2,
+            dim_hidden = dim_hidden,
+            dim_out = 3,
+            num_layers = 6
+        )
+
+    def forward(self, latent, coors):
+        gamma, beta = self.mapping(latent)
+        return self.siren(coors, gamma, beta)
+
+class piGAN(nn.Module):
+    def __init__(self, image_size, dim, dim_hidden):
+        super().__init__()
+        self.image_size = image_size
+
+        coors = torch.stack(torch.meshgrid(
+            torch.arange(image_size),
+            torch.arange(image_size)
+        ))
+
+        coors = rearrange(coors, 'c h w -> (h w) c')
+        self.register_buffer('coors', coors)
+
+        self.G = Generator(
+            image_size = image_size,
+            dim = dim,
+            dim_hidden = dim_hidden
+        )
+
+    def forward(self, x):
+        device, b = x.device, x.shape[0]
+        coors = repeat(self.coors, 'n c -> b n c', b = b).float()
+        return self.G(x, coors)
